3ba287b508ee97d2170fed214193c3ea1e0f2d7e
MarkSaba7 <marksaba76@gmail.com>
2025-03-30 10:43:18 -0400

    ajoute soumettre distance

--- public/index.js
+++ public/index.js
@@ -1,8 +1,15 @@
+const tabs = []
+
+let tabOuverte = 0
+
+
+
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
+        tabs.push(tabcontent[i].id)
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
@@ -23,21 +30,10 @@ function clearDistanceForm() {
 }
 
 document.getElementById("speedForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    
+    e.preventDefault(); 
 
-    let speed = document.getElementById("speed").value;
-    let angle = document.getElementById("angle").value;
-    const vitesseValidee = validerNombre(speed);
-    if (!vitesseValidee.success) { 
-        montrerMessage(false, "La vitesse n'est pas une valeur")
-        return;
-    }
-    const angleValide = validerNombre(angle);
-    if (!angleValide.success) { 
-        montrerMessage(false, "L'angle n'est pas une valeur")
-        return;
-    }
+    const speed = parseFloat(document.getElementById("speed").value);
+    const angle = parseFloat(document.getElementById("angle").value);
 
     const reponse = await fetch("http://localhost:8080/api/soumettre-vitesse", {
         method: "POST", 
@@ -45,8 +41,8 @@ document.getElementById("speedForm").addEventListener("submit", async (e) => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            vitesse: vitesseValidee.valeur, 
-            angle: angleValide.valeur
+            vitesse: speed, 
+            angle: angle
         })
     });
     if (!reponse.ok) {
@@ -58,15 +54,26 @@ document.getElementById("speedForm").addEventListener("submit", async (e) => {
     
 });
 
-document.getElementById("distanceForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    var distance = document.getElementById("distance").value;
-    var confirmation = document.getElementById("distanceConfirmation");
-    
-    confirmation.innerHTML = "Paramètre validé:<br>Distance finale: " + distance + " cm";
-    confirmation.style.display = "block";
-    
-    confirmation.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+document.getElementById("distanceForm").addEventListener("submit", async (e) => {
+    e.preventDefault(); 
+
+    const distance = parseFloat(document.getElementById("distance").value);
+    console.log(distance)
+
+    const reponse = await fetch("http://localhost:8080/api/soumettre-distance", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            distance: distance
+        })
+    });
+    if (!reponse.ok) {
+        montrerMessage(false, "Le serveur a repondu avec: " + reponse.status);
+        return;
+    }
+    montrerMessage(true, "Les informations etaient envoyee au serveur");
 });
 
 function validerNombre(str) {
@@ -88,9 +95,11 @@ function montrerMessage(success, message) {
     confirmation.style.display = "block";
     if (success) {
         confirmation.style.border = "1px solid #4CAF50";
+        confirmation.style.backgroundColor = "#dff0d8";
         confirmation.style.color = "#3c763d";
     } else {
         confirmation.style.border = "1px solid rgb(185, 17, 17)";
+        confirmation.style.backgroundColor = "#ff9f9f";
         confirmation.style.color = "#ff0000";
     }
     confirmation.innerHTML = message;
--- src/endpoint/API.ts
+++ src/endpoint/API.ts
@@ -3,11 +3,20 @@ import express, {Request, Response} from "express";
 export default class APIEndpoint {
     constructor(serveur: express.Express){
         serveur.post("/api/soumettre-vitesse", (req, rep) => this.submitVitesse(req, rep));
+        serveur.post("/api/soumettre-distance", (req, rep) => this.submitDistance(req, rep));
     }
 
     private submitVitesse(req: Request, rep: Response): void {
-        const requete = req.body as RequeteVitesse
-        console.log(requete)
+        const requete = req.body as RequeteVitesse;
+        console.log("/api/soumettre-vitesse")
+        console.log(requete);
+        rep.status(200).send();
+    }
+
+    private submitDistance(req: Request, rep: Response): void {
+        const requete = req.body as RequeteDistance;
+        console.log("/api/soumettre-distance")
+        console.log(requete);
         rep.status(200).send();
     }
 }
@@ -15,4 +24,8 @@ export default class APIEndpoint {
 type RequeteVitesse = {
     vitesse: number;
     angle: number;
+}
+
+type RequeteDistance = {
+    distance: number;
 }
